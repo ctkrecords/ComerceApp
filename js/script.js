ac89f145143eda44ddace69c0609baf588f6fb89
ctkrecords <ctkrecordings@gmail.com>
2019-07-14 08:36:04 -0600

    Se agregó todo el proyecto

---
+++ js/script.js
@@ -0,0 +1,99 @@
+function ocultar_mostrar() {
+  var x = document.getElementById('mas');
+  var y = document.getElementById('menos');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar1() {
+  var x = document.getElementById('mas1');
+  var y = document.getElementById('menos1');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar2() {
+  var x = document.getElementById('mas2');
+  var y = document.getElementById('menos2');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar3() {
+  var x = document.getElementById('mas3');
+  var y = document.getElementById('menos3');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar4() {
+  var x = document.getElementById('mas4');
+  var y = document.getElementById('menos4');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar5() {
+  var x = document.getElementById('mas5');
+  var y = document.getElementById('menos5');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar6() {
+  var x = document.getElementById('mas6');
+  var y = document.getElementById('menos6');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar7() {
+  var x = document.getElementById('mas7');
+  var y = document.getElementById('menos7');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
+function ocultar_mostrar8() {
+  var x = document.getElementById('mas8');
+  var y = document.getElementById('menos8');
+  if (x.style.display === 'block') {
+    y.style.display = 'block';
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'block';
+    y.style.display = 'none';
+  }
+}
